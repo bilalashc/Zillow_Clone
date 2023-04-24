a4a93d4765ffd2c5c98ba7279146347cae4eea13
Bilal Choudhry <bilalachoudhry@gmail.com>
2023-04-23 21:41:25 -0700

    UA: Frontend Login

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,6 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
-  end
-
-  post 'api/test', to: 'application#test'
-  
+  end  
 end
 
