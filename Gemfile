e5f1c926e23b270262830ae8454d5530f73735ba
Bilal Choudhry <bilalachoudhry@gmail.com>
2023-04-26 23:34:28 -0700

    Listings Seed Data & AWS Setup

--- Gemfile
+++ Gemfile
@@ -40,7 +40,6 @@ gem "aws-sdk-s3"
 gem "bcrypt"
 gem "jbuilder"
 gem "faker"
-gem "annotate"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 require "open-uri"
 
 ApplicationRecord.transaction do 
-# More users
+
+User.destroy_all   
+Listing.destroy_all
+
+ApplicationRecord.connection.reset_pk_sequence!('users')
+ApplicationRecord.connection.reset_pk_sequence!('listings')
+User.create!( 
+    email: 'bilal@gmail.com', 
+    password: 'ashfaque'
+)
+
+#More Users
   10.times do 
     User.create!({
       email: Faker::Internet.unique.email,
@@ -9,7 +20,7 @@ ApplicationRecord.transaction do
     }) 
   end
 
-  20.times do
+  30.times do
     Listing.create!(
       address: Faker::Address.street_address,
       street: Faker::Address.street_name,
@@ -17,9 +28,12 @@ ApplicationRecord.transaction do
       state: 'California',
       zip_code: Faker::Address.zip_code,
       market_status: ['For Sale', 'Off-Market'].sample,
-      home_price: Faker::Number.between(from: 500000, to: 2000000),
-      rent_estimate: Faker::Number.between(from: 1000, to: 5000),
+      home_price: Faker::Number.between(from: 1000000, to: 5000000),
+      rent_estimate: Faker::Number.between(from: 10000, to: 50000),
       home_overview: Faker::Lorem.paragraphs.join('\n'),
+      bedrooms: Faker::Number.between(from: 1, to: 6),
+      bathrooms: Faker::Number.between(from: 1, to: 6),
+      listing_size: Faker::Number.between(from: 1000, to: 10000),
       author: User.all.sample
     )
   end
