306cb5097e1c5c0eb0b64e0bc413e60f47f9c992
Bilal Choudhry <bilalachoudhry@gmail.com>
2023-06-15 02:18:19 +0500

    refactor the code

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -10,24 +10,11 @@ export default function Footer() {
   return (
     <div className="footer_container">
       <div className="footer_company_details">
-        <img
-          style={{ height: "30px", marginBottom: "20px" }}
-          src="https://s.zillowstatic.com/pfs/static/z-logo-default.svg"
-          className="logo"
-          alt="Logo"
-        />
-
-        <h5>copyright@2023</h5>
-      </div>
-      <div className="footer_about">
-
-        <h5>About</h5>
-        <p style={{cursor: "pointer"}} onClick={()=> navigate("/sell")}>Sell</p>
+      <h5>About</h5>
       </div>
       <div >
-        <a  style={{ marginRight: "20px" }} target="_blank" href="https://www.facebook.com"><FacebookIcon htmlColor={"black"}/></a>
-        <a  style={{ marginRight: "20px" }} target="_blank" href="https://linkedin.com"><LinkedInIcon htmlColor={"black"}/></a>
-        <a href="https://github.com" target="_blank"><GitHubIcon htmlColor={"black"}/></a>
+        <a  style={{ marginRight: "20px" }} target="_blank" href="https://www.linkedin.com/in/bilal-c-838805b4"><LinkedInIcon htmlColor={"black"}/></a>
+        <a href="https://github.com/bilalashc" target="_blank"><GitHubIcon htmlColor={"black"}/></a>
       </div>
     </div>
   );
